5baa6c76b64b54eb5c40d56ae7e667d7b29eada2
Nicolae Darie-Nistor <memedarie@gmail.com>
2023-04-09 16:19:32 +0200

    Initial Commit

---
+++ SwiftConcurrency/SwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyApp.swift
+//  SwiftConcurrency
+//
+//  Created by Darie-Nistor Nicolae on 09.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
